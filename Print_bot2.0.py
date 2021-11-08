5b7080d4b5e098f03014248f0bffcd57cbfeef6f
IsaiasAraujo <isaiasaraujopi@gmail.com>
2021-11-08 18:02:19 -0300

    master

--- Print_bot2.0.py
+++ Print_bot2.0.py
@@ -1,21 +1,18 @@
-#import pathlib
-#import win32print
+import win32print
 import win32api
 import os
 from time import sleep
 from pathlib import Path
 
 #Colocar aqui o comando para o python Criar uma pasta
-cwd = os.getcwd()
+caminho= os.getcwd()+'\imprimir'
 try:
-    os.mkdir(cwd+'\\'+'imprimir')
-    
+    os.mkdir(caminho+'\\'+'imprimir')    
 except:
-   
-   
-
+    print('Pasta imprimir já existe') 
+#lista_arquivos=os.listdir(caminho)
 while 0==0:
-    #lista_arquivos = os.listdir(caminho)
+    lista_arquivos = os.listdir(caminho)
     if lista_arquivos != []: 
         for arquivo in lista_arquivos:
             win32api.ShellExecute(0, "print", arquivo, None, caminho, 0)
