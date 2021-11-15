28b6f5ef1b55f14d3b65fd99de3e604b71bd4ac9
IsaiasAraujo <isaiasaraujopi@gmail.com>
2021-11-15 16:31:06 -0300

    master

--- PrintBot/teste.py
+++
@@ -1,3 +0,0 @@
-import os
-caminho='c:'
-os.listdir('')
--- Print_bot2.0.py
+++ Print_bot2.0.py
@@ -5,17 +5,26 @@ from time import sleep
 from pathlib import Path
 
 #Colocar aqui o comando para o python Criar uma pasta
-caminho= os.getcwd()+'\imprimir'
+caminho= os.getcwd()
+caminho_imp=caminho+'\\'+'imprimir'
+
 try:
-    os.mkdir(caminho)    
+    os.mkdir(caminho_imp)
+    print('Criando pasta Imprimir')    
 except:
     print('Pasta imprimir já existe') 
 #lista_arquivos=os.listdir(caminho)
 while 0==0:
-    lista_arquivos = os.listdir(caminho)
+    lista_arquivos = os.listdir(caminho_imp)
     if lista_arquivos != []: 
         for arquivo in lista_arquivos:
-            #win32api.ShellExecute(0, "print", arquivo, None, caminho, 0)
+            print(arquivo)
+            win32api.ShellExecute(0, "print", arquivo, None, caminho_imp, 0)
             sleep(5)
-            os.remove(caminho+'\\'+arquivo)
+            try:
+                os.remove(caminho_imp+'\\'+arquivo)
+            except PermissionError as erro:
+                sleep(10)
+                os.remove(caminho_imp+'\\'+arquivo)
             sleep (5)
+            
