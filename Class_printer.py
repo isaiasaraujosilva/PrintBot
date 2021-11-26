285f88ac6d5013d84bf48556834d0965558ee0f9
IsaiasAraujo <isaiasaraujopi@gmail.com>
2021-11-26 17:34:18 -0300

    master

--- Class_printer.py
+++ Class_printer.py
@@ -6,6 +6,18 @@ import win32print
 
 class Printer:
     
+    def setImpEtiqueta(prtCodeEtiqueta):
+        prtCodeEtiqueta=int(prtCodeEtiqueta)
+        list_imp=win32print.EnumPrinters(2)
+        imp=list_imp[prtCodeEtiqueta]
+        win32print.SetDefaultPrinter(imp[2])
+        return print('impressora de etiqueta selecionada: {}'.format(imp[2] ))
+    def setImpA4(prtCodeA4):
+        prtCodeA4=int(prtCodeA4)
+        list_imp=win32print.EnumPrinters(2)
+        imp2=list_imp[prtCodeA4]
+        win32print.SetDefaultPrinter(imp2[2])
+        return print('impressora de A4 selecionada: {}'.format(imp2[2] ))
     def getListPrinter():
         list_printer = win32print.EnumPrinters(2)
         return list_printer
--- Print_bot2.0.py
+++ Print_bot2.0.py
@@ -11,14 +11,27 @@ for p in printer.getListPrinter():
     genCode=genCode+1
     p1=p 
     print(' ---> {} {}'.format(genCode,p1[2]))
-prtCode=input('Digite o numero da impressora de ETIQUETA: ')    
+prtCodeEtiqueta=input('Digite o numero da impressora de ETIQUETA: ')
+printer.setImpEtiqueta(prtCodeEtiqueta)
+prtCodeA4=input('Digite o numero da impressora de A4: ')
+printer.setImpA4(prtCodeA4)
+print('\n \n Em Execução...')
 while True:  
     if system.fileList() != []: 
         for i in system.fileList():
             print(i)
             #win32api.ShellExecute(0, "print", arquivo, None, caminho_imp, 0)
-            printer.sendPrint(i,system.getPrintFolder())
-            sleep(5)
+            if i == 'hd.pdf':
+                printer.setImpEtiqueta(prtCodeEtiqueta)
+                sleep(2)
+                printer.sendPrint('hd_inservivel.pdf',system.getMainFolder())
+                sleep(2)
+                printer.setImpA4(prtCodeA4)
+                printer.sendPrint(i,system.getPrintFolder())
+            else:   
+                printer.sendPrint(i,system.getPrintFolder())
+                sleep(5)
+            sleep(5)    
             system.remover(i)
             
            
