4f02fb9841e57280c22e729337f6f9ddd183889a
IsaiasAraujo <isaiasaraujopi@gmail.com>
2022-08-09 21:58:03 -0300

    implementação do monitoramento da fila de impressão

--- Class_printer.py
+++ Class_printer.py
@@ -12,18 +12,25 @@ class Printer:
         imp=list_imp[prtCodeEtiqueta]
         win32print.SetDefaultPrinter(imp[2])
         return print('impressora de etiqueta selecionada: {}'.format(imp[2] ))
+
     def setImpA4(prtCodeA4):
         prtCodeA4=int(prtCodeA4)
         list_imp=win32print.EnumPrinters(2)
         imp2=list_imp[prtCodeA4]
         win32print.SetDefaultPrinter(imp2[2])
+        win32print.OpenPrinter(imp2[2])
         return print('impressora de A4 selecionada: {}'.format(imp2[2] ))
+
     def getListPrinter():
         list_printer = win32print.EnumPrinters(2)
         return list_printer
+
     def sendPrint(file,fileFolder):
-        win32api.ShellExecute(0, "print", file, None, fileFolder, 0)
-        return print('Aquivo enviado para a impressora: {} '.format(file))
-        
-#definir impressora
-#definir metodo de impressão 
+        try:
+            win32api.ShellExecute(0, "print", file, None, fileFolder, 0)
+            return print('Aquivo enviado para a impressora: {} '.format(file))
+        except:
+            return print("Erro: verifique o Foxit");
+
+
+  
--- Print_bot2.0.py
+++ Print_bot2.0.py
@@ -2,37 +2,33 @@ from os import system
 from Class_os import System
 from time import sleep
 from Class_printer import Printer
+import win32print
+
 system=System
 system.createFolder('imprimir')
 printer=Printer
 print('Lista de impressoras instaladas no sistema:')
 genCode=-1
+
 for p in printer.getListPrinter():
     genCode=genCode+1
     p1=p 
     print(' ---> {} {}'.format(genCode,p1[2]))
-prtCodeEtiqueta=input('Digite o numero da impressora de ETIQUETA: ')
-printer.setImpEtiqueta(prtCodeEtiqueta)
+
 prtCodeA4=input('Digite o numero da impressora de A4: ')
 printer.setImpA4(prtCodeA4)
 print('\n \n Em Execução...')
+
 while True:  
     if system.fileList() != []: 
         for i in system.fileList():
             print(i)
             #win32api.ShellExecute(0, "print", arquivo, None, caminho_imp, 0)
-            if i =='hd.pdf' or i=='Hd.pdf' or i=='hD.pdf'or i=='HD.pdf':
-                printer.setImpEtiqueta(prtCodeEtiqueta)
-                sleep(2)
-                printer.sendPrint('hd_inservivel.pdf',system.getMainFolder())
-                sleep(2)
-                printer.setImpA4(prtCodeA4)
-                printer.sendPrint(i,system.getPrintFolder())
-            else:   
-                printer.sendPrint(i,system.getPrintFolder())
-                sleep(5)
+            printer.sendPrint(i,system.getPrintFolder())
+            print_jobs = win32print.EnumJobs(,0, -1, 1)
+            print(print_jobs)
             sleep(5)    
             system.remover(i)
-            
+                        
            
             
